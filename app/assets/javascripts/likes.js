bf6d7cfac064ea3339367e76ce06bd13115cebcc
Cyril Chehovah <c.chehovah@gmail.com>
2016-05-18 21:41:15 +0200

    starting to work on ad display

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,2 +1,9 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// // Place all the behaviors and hooks related to the matching controller here.
+// // All this logic will automatically be available in application.js.
+
+// $("#watch_ad").on('click', function() {
+//   $('#post_category, #preview_category').val($(this).text());
+//   $('.post-category').removeClass('active');
+//   $(this).addClass('active');
+//   $("#submit_url").prop('disabled', false);
+// });
